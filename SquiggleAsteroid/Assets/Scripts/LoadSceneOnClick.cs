0d1fc3b52d2e33b03591e632cf8b3de6d6e720fc
charlieefde <46528533+charlieefde@users.noreply.github.com>
2019-01-11 01:44:11 +0000

    Add files via upload

---
+++ SquiggleAsteroid/Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//Loads game scene from main menu
+public class LoadSceneOnClick : MonoBehaviour { 
+
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+}
